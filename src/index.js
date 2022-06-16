2bf6aa013557c6c8164a60ba74581b91b364319d
aliceauko <alice.auko@student.moringaschool.com>
2022-06-16 22:32:39 +0300

    commit tree b3052544a6fbcdba7afc31f3ba62da884b6520f3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    dogsUrl();
+    urlBreeds();
+  })
+
+//challenge 1
+function dogsUrl(){
+   fetch (imgUrl)
+   .then (response => response.json())
+   .then((data) => {
+    data.message.forEach(dog=> {
+        const contain = document.getElementById('dog-image-container');
+        const img = document.createElement('img'); 
+        img.setAttribute("src",dog )
+        contain.appendChild(img);
+    });
+   })
+
+  }
+
+
+
+
+//challenge 2 ,3
+
+function urlBreeds(){
+const renderImages=(images) => {
+    const ul = document.getElementById('dog-breeds');
+    loadAllBreeds(results,ul);
+    document.getElementById('breed-dropdown').addEventListener("change",()=>{
+        let dropdownValue = document.getElementById('breed-dropdown').value;
+        let filterDog = result.filter((array)=>{
+            return array.startsWith(dropdownValue);
+         })
+         console.log(filterDog);
+         loadAllBreeds(filterDog,ul);
+
+    })
+   };
+
+
+const fetchImages = async ()=> {
+    const response = await fetch(breedUrl);
+    const data = await response.json ()
+    console.log (data)
+    const results = data.results;
+
+    results.map((images) => renderImages(images));
+    console.log (results);
+
+}
+
+fetchImages();
+
+}
